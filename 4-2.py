7a01e86a2ea505ca4c7645e7eb78d9381562f442
Anna Beatriz Chaboudet Chazan <122185337+annabchazan@users.noreply.github.com>
2023-05-08 19:45:38 -0300

    Create 4-2.py

---
+++ 4-2.py
@@ -0,0 +1,20 @@
+x=int(input('x'))
+passos=0
+while(x>0):
+    if(x>5):
+        x=x-5
+        passos+=1
+    elif(x>4):
+        x=x-4
+
+    elif(x>3):
+        x=x-3
+        passos += 1
+
+    elif(x>2):
+        x=x-2
+        passos += 1
+    elif(x>1):
+        x=x-1
+        passos +=1
+print(passos)
